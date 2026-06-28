5f7a8dd17d837f2083de034fb1fc599f4e648541
santi1029 <santimarco051@gmail.com>
2026-06-28 12:38:00 +0200

    Change static file serving and add home route
    Updated static file serving to use the root directory and added a route for the index.html file.

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-// Dice al server di servire i file statici (HTML, CSS, JS) dalla cartella "public"
-app.use(express.static(path.join(__dirname, 'public')));
+// Dice al server di cercare i file statici direttamente nella cartella principale
+app.use(express.static(__dirname));
+
+// Quando qualcuno va sulla Home (/), gli manda il file index.html dalla root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server attivo sulla porta ${PORT}`);
